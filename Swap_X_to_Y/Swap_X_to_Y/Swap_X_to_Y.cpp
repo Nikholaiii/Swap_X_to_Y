b5130e8bf363cf126fae70ee73e8ea19647e2782
Nikholaiii <57456813+Nikholaiii@users.noreply.github.com>
2019-12-06 12:02:41 +0300

    first commit
    i throw my first commit on git.

---
+++ Swap_X_to_Y/Swap_X_to_Y/Swap_X_to_Y.cpp
@@ -0,0 +1,19 @@
+﻿#include <string>
+#include <ctime>
+using namespace std;
+int main()
+{
+	setlocale(LC_ALL, "ru");
+	srand(time(NULL));
+	int x;
+	int y;
+	x = 50;
+	y = 20;
+	cout << "X =  " << x << "\ty =  " << y << endl;
+	y = x + y;
+	x = y - x;
+	y = y - x;
+	cout << "X =  " << x << "\ty =  " << y << endl;
+	system("pause");
+	return 0;
+}
